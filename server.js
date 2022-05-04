5d8190c1c4562103d1d25d4cf3e025917d17fed4
Itzamary Jimenez <itzamary1gonzalez@hotmail.com>
2022-05-04 16:12:48 -0500

    added folder to incorect directory moved it to correnct location

--- server.js
+++ server.js
@@ -115,6 +115,11 @@ app.post('/api/animals', (req, res) => {
   
 });
 
+// get request for our index.html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
 });
