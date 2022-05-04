fe9a1c11c982cd9af42bafcc63739a3422dbce57
Itzamary Jimenez <itzamary1gonzalez@hotmail.com>
2022-05-04 08:22:20 -0500

    minor change to reverse to how it was on line 57

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ function filterByQuery(query, animalsArray) {
     return result;
   }
 
-app.get('/', (req, res) => {
+app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query){
         results = filterByQuery(req.query, results);    
